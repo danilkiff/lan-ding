6cac3ead5c717532fce72cf90a4b469e48d279be
Oleg Y. Danilkiff <13948753+danilkiff@users.noreply.github.com>
2025-03-02 01:38:58 +0300

    Add Airflow

--- src/stores/links.js
+++ src/stores/links.js
@@ -13,6 +13,7 @@ export const useLinksStore = defineStore('links', {
     
       // ai & ds
       { url: 'https://n8n.z.pq3.ru', name: 'n8n', icon: '/icons/n8n.svg', category: 'AI & DS', available: true },
+      { url: 'https://airflow.z.pq3.ru', name: 'Airflow', icon: '/icons/airflow.svg', category: 'AI & DS', available: true },
     ]
   }),
 
