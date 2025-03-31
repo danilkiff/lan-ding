8f9087d457f313bd5b8300323616e1c2208a156f
Oleg Y. Danilkiff <13948753+danilkiff@users.noreply.github.com>
2025-03-31 23:34:36 +0300

    Add langflow

--- src/stores/links.js
+++ src/stores/links.js
@@ -15,6 +15,7 @@ export const useLinksStore = defineStore('links', {
       // ai & ds
       { url: 'https://n8n.z.pq3.ru', name: 'n8n', icon: '/icons/n8n.svg', category: 'AI & DS', available: true },
       { url: 'https://airflow.z.pq3.ru', name: 'Airflow', icon: '/icons/airflow.svg', category: 'AI & DS', available: true },
+      { url: 'https://langflow.z.pq3.ru', name: 'Langflow', icon: '/icons/langflow.svg', category: 'AI & DS', available: true },
     ]
   }),
 
