8844f541a015133ee676a28125fddfdbeec917ce
Oleg Y. Danilkiff <13948753+danilkiff@users.noreply.github.com>
2025-06-25 15:39:20 +0300

    fix(layout): center grid tiles when few items are present
    Wrapped the .grid in a .grid-wrapper with flex centering and replaced
    `auto-fill` with `auto-fit` + `max-content` to prevent grid from stretching.
    Also removed `width: 100%` to allow natural shrink-to-fit behavior.

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,8 @@
         {{ category }}
       </span>
     </div>
-
-    <div class="grid">
+    
+    <div class="grid-wrapper"><div class="grid">
       <div v-for="link in filteredLinks" :key="link.url" class="tile" :class="{ unavailable: !link.available }">
         <a v-if="link.available" :href="link.url" target="_blank">
           <img :src="link.icon" :alt="link.name" />
@@ -22,7 +22,7 @@
           <p>{{ link.name }}</p>
         </div>
       </div>
-    </div>
+    </div></div>
   </div>
 </template>
 
